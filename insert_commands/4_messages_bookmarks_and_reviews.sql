becbab76722d7d57f768470dedf9f0ef8f593dd0
sanan <sanan@techgrowthkw.com>
2025-05-28 16:07:51 +0330

    feat(queries): merge messages inserts with bookmarks and reviews

--- insert_commands/4_messages_bookmarks_and_reviews.sql
+++ insert_commands/4_messages_bookmarks_and_reviews.sql
@@ -27,3 +27,23 @@ INSERT INTO message (content, senddate, isread, senderid, receiverid, adid) VALU
 ('Hey! Are the Coldplay tickets still available?', NOW() - INTERVAL '1 day', false, 1, 9, 19),
 ('Yes! They are for next Saturday’s show. Front section.', NOW() - INTERVAL '1 day', false, 9, 1, 19),
 ('Awesome. I’ll take them. Can we meet today?', NOW(), false, 1, 9, 19);
+
+---------------------------------------------------------------------------------------------------------------------
+-- Bookmark
+INSERT INTO Bookmark (BookmarkDate, UserID, AdID)
+VALUES
+(NOW(), 1, 1),
+(NOW() - INTERVAL '1 day', 1, 2),
+(NOW() - INTERVAL '2 days', 3, 1),
+(NOW() - INTERVAL '1 days', 4, 1);
+
+---------------------------------------------------------------------------------------------------------------------
+-- Review
+INSERT INTO Review (Rating, Comment, ReviewDate, ReviewerID, TargetUserID)
+VALUES
+(5, 'Great seller!', NOW(), 1, 2),
+(4, 'Has great prices.', NOW(), 1, 3),
+(3, 'Medium quality stuff!', NOW(), 1, 3),
+(1, 'Not a nice attitude...', NOW(), 4, 1),
+(2, 'Not a nice attitude...', NOW(), 3, 1),
+(1, 'Bad seller! :(', NOW(), 4, 4);
